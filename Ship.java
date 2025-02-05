cb5ad42cbc08ff0ca5f06423fea2ec9db9b69a74
wcheng418 <wilson.cheng.007@gmail.com>
2025-02-04 19:09:29 -0600

    I made the comment

--- Ship.java
+++ Ship.java
@@ -13,22 +13,27 @@ abstract class Ship implements Displayable {
     public Ship(String name, String yearBuilt) {
         setName(name);
         setyearBuilt(yearBuilt);
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
     public String getName() {
         return name;
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
     public String getYearBuilt() {
         return yearBuilt;
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
     public void setName(String name) {
         this.name = name;
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
     public void setyearBuilt(String yearBuilt) {
         this.yearBuilt = yearBuilt;
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
     //toString
@@ -37,6 +42,12 @@ abstract class Ship implements Displayable {
         String str = "The ship name: " + getName()
                 + "\nThe year it was built: " + getYearBuilt();
         return str;
+        //this change is made by Wilson Cheng 02/04/2025
+        //this change is made by Wilson Cheng 02/04/2025
+        //this change is made by Wilson Cheng 02/04/2025
+        //this change is made by Wilson Cheng 02/04/2025
+        //this change is made by Wilson Cheng 02/04/2025
+        //this change is made by Wilson Cheng 02/04/2025
     }
 
 }
