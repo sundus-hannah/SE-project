47c6e60ac0218c35f9169e72dff60b4d2d027ae5
wcheng418 <wilson.cheng.007@gmail.com>
2025-02-06 18:52:16 -0600

    another change

--- Ship.java
+++ Ship.java
@@ -13,35 +13,30 @@ abstract class Ship implements Displayable {
     public Ship(String name, String yearBuilt) {
         setName(name);
         setyearBuilt(yearBuilt);
-        System.out.println("this change is made by Wilson Cheng 02/04/2025 in the Ship");
     }
 
     public String getName() {
-        System.out.println("this change is made by Wilson Cheng 02/04/2025 in the getName");
         return name;
     }
 
     public String getYearBuilt() {
-        System.out.println("this change is made by Wilson Cheng 02/04/2025 in the getyearBuilt");
         return yearBuilt;
     }
 
     public void setName(String name) {
         this.name = name;
-        System.out.println("this change is made by Wilson Cheng 02/04/2025 in the setName");
     }
 
     public void setyearBuilt(String yearBuilt) {
         this.yearBuilt = yearBuilt;
         //
-        System.out.println("this change is made by Wilson Cheng 02/04/2025 in the setyearBuilt");
     }
 
     //toString
     @Override
     public String toString() {
         String str = "The ship name: " + getName()
-                + "\nThe year it was built: " + getYearBuilt() + "\tthis change is made by Wilson Cheng 02/04/2025";
+                + "\nThe year it was built: " + getYearBuilt() + "\tthis snippet was made tampered by Wilson Cheng 02/04/2025";
         return str;
 
     }
